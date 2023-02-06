dd38ccd1e5be0bac3a649f2bd53a31ac01344dee
Opwsprin <36404709+Opwsprin@users.noreply.github.com>
2023-02-06 21:31:07 +0000

    complete (tricky) excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    for (item of items){
+        for (i=0;i<array.length;i++){
+            if (array[i]===item){
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
